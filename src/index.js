f8c84496b977c85d274a316c1e5ca92b1a6daa2d
bonsa <bonsa>
2024-02-19 23:33:11 +0300

    the test

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import template from './index.html';
+import template_style from './style.css';
+
+const elemnt =document.querySelector(".test");
+// elemnt.style.opacity="0"
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html', // Path to your HTML template file
+        }),
+      ],
+    devtool: 'inline-source-map', 
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.html$/,
+            use: 'html-loader',
+          },
+        ],
+      },
+  };
